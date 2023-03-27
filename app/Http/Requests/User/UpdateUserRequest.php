8dd5936369d8535c79a6af50510dea2d3648789a
Filbert Mangiri <filbert.mangiri@student.umn.ac.id>
2023-03-27 08:36:08 +0700

    Update UpdateUserRequest.php

--- app/Http/Requests/User/UpdateUserRequest.php
+++ app/Http/Requests/User/UpdateUserRequest.php
@@ -24,7 +24,7 @@ class UpdateUserRequest extends FormRequest
 		$this->merge([
 			'department_id' => $this->department['value'] ?? null,
 			'sub_department_id' => $this->sub_department['value'] ?? null,
-			'roles' => array_map(fn ($role) => $role['label'], $this->roles),
+			'roles' => $this->roles ? array_map(fn ($role) => $role['label'], $this->roles) : [],
 		]);
 	}
 
