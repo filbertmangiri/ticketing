82dc4d6e35be520026c73bf8e28ddef76cbf3f13
Filbert Mangiri <filbert.mangiri@student.umn.ac.id>
2023-03-27 08:30:12 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,7 +23,7 @@ class DatabaseSeeder extends Seeder
 			PermissionSeeder::class,
 		]);
 
-		if (env('APP_DEBUG', true)) {
+		if (env('APP_DEBUG', true) == true) {
 			$this->call([
 				DepartmentSeeder::class,
 				SubDepartmentSeeder::class,
